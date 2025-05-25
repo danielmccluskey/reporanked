75c59dc72c08dab54ca8f3a417031b6f44cd184f
Daniel McCluskey <dan@cheltenhaminteractive.com>
2025-05-25 14:23:12 +0100

    Update version to 0.9.6 for RepoRanked plugin
    Bump the version number in the BepInPlugin attribute from "0.9.5" to "0.9.6". This release may include new features, bug fixes, or improvements.

--- RepoRanked.cs
+++ RepoRanked.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 
 namespace RepoRanked;
 
-[BepInPlugin("danos.RepoRanked", "RepoRanked", "0.9.5")]
+[BepInPlugin("danos.RepoRanked", "RepoRanked", "0.9.6")]
 [BepInDependency("nickklmao.menulib", BepInDependency.DependencyFlags.HardDependency)]
 
 public class RepoRanked : BaseUnityPlugin
